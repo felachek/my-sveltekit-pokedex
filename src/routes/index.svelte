f4a35c001cae317dc7cce8b26fc097ab2f6d7f3a
Farouk El Achek <felachek@gmail.com>
2021-12-21 23:43:42 -0500

    added tailwind, svelte-store and api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ page }) {
+		const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+		const res = await fetch(url);
+		const data = await res.json();
+		const loadedPokemon = data.results.map((data, index) => {
+			return {
+				name: data.name,
+				id: index + 1,
+				image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+					index + 1
+				}.png`
+			};
+		});
+		return { props: { pokemons: loadedPokemon } };
+	}
+</script>
+
+<script>
+	import PokemonCard from '../components/pokemonCard.svelte';
+	export let pokemons;
+
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = pokemons.filter((pokemon) =>
+				pokemon.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+			);
+		} else {
+			filteredPokemon = [...pokemons];
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	placeholder="Search Pokemon"
+	bind:value={searchTerm}
+/>
+
+<div class="py-4 grid gap-3 md:grid-cols-2 grid-cols-1">
+	{#each filteredPokemon as pokemon}
+		<PokemonCard {pokemon} />
+	{/each}
+</div>
